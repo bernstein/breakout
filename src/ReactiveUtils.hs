aa23008aabed289e58d3b36851fd52ad162fbd52
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2012-07-28 14:32:43 +0200

    Suggestion: I find the infix operators <@> and <@ to be more pleasant than the `snapshot` family of functions.

--- src/ReactiveUtils.hs
+++ src/ReactiveUtils.hs
@@ -24,9 +24,6 @@ module ReactiveUtils
        , withPrevE
        , withPrevEWith
        , diffE
-       , snapshotWith
-       , snapshot
-       , snapshot_
        , unique
        , once
        ) where
@@ -37,15 +34,12 @@ import Data.AffineSpace
 import Data.Active (fromDuration, Time)
 
 integral :: (VectorSpace v, Scalar v ~ Double) => Event t Time -> Behavior t v -> Behavior t v
-integral t b = sumB (snapshotWith (*^) b (fromDuration <$> diffE t))
+integral t b = sumB $ (\v dt -> fromDuration dt *^ v) <$> b <@> diffE t
 
 sumB :: AdditiveGroup a => Event t a -> Behavior t a
 sumB = accumB zeroV . fmap (^+^)
 
 withPrevE :: Event t a -> Event t (a,a)
-withPrevE = filterJust . fmap f . accumE (Nothing,Nothing) . fmap ((\new (prev,_) -> (new,prev)).Just)
-  where
-    f :: (Maybe a, Maybe b) -> Maybe (a,b)
 withPrevE = withPrevEWith (,)
 
 withPrevEWith :: (a -> a -> b) -> Event t a -> Event t b
@@ -56,15 +50,6 @@ withPrevEWith f e = filterJust . fst . mapAccum Nothing $ g <$> e
 
 diffE :: AffineSpace a => Event t a -> Event t (Diff a)
 diffE = withPrevEWith (.-.)
-
-snapshotWith :: (a -> b -> c) -> Behavior t b -> Event t a -> Event t c
-snapshotWith h b e = (flip h <$> b) `apply` e
-
-snapshot :: Behavior t b -> Event t a -> Event t (a,b)
-snapshot = snapshotWith (,)
-
-snapshot_ :: Behavior t b -> Event t a -> Event t b
-snapshot_ = snapshotWith (flip const)
 
 unique :: Eq a => Event t a -> Event t a
 unique = filterJust . accumE Nothing . fmap (\a acc -> if Just a == acc then Nothing else Just a)
